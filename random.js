db4d6adcff351a0585d58fb6f3a92c4f12b4ba15
KanyeWestVan <125940103+KanyeWestVan@users.noreply.github.com>
2023-11-29 09:28:36 -0500

    Create random.js

---
+++ random.js
@@ -0,0 +1,12 @@
+        function Randomer(max) {
+          return Math.floor(Math.random() * max);
+        }; 
+//loop Randomer or put randomer in tester4 
+        function tester4(){
+            let cords = {
+              left1:Randomer(1004),
+              right1:Randomer(1342),
+              bottom1:Randomer(500),
+              top1:Randomer(453)
+            }
+          };
